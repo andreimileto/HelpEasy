6ef31072486246a6cf7a9831c1847600caebe486
andreimileto <andreimileto@hotmail.com>
2017-08-07 21:20:26 -0300

    V0.03
    -Configuração Hibernate
    -Criação classes
    -Criação interface a ser implementada nos DAO(sujeito a ajustes)

---
+++ src/DAO/ProjetoDAO.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DAO;
+
+import apoio.IDAO;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Mileto
+ */
+public class ProjetoDAO implements IDAO{
+
+    @Override
+    public boolean salvar(Object o) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public ArrayList<Object> consultar(Object o) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
