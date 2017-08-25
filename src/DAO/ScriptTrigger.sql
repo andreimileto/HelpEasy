c6ef0bb69057578b356d1dfbe789c9bd8ca3ea27
vogelleandro <vogelleandro@gmail.com>
2017-08-25 16:20:37 -0300

    V0.07
    Criação do Scripts da Trigger.

---
+++ src/DAO/ScriptTrigger.sql
@@ -0,0 +1,22 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ * Author:  User
+ * Created: 25/08/2017
+ */
+
+CREATE TRIGGER trgAuditoriaCidade AFTER INSERT OR UPDATE ON Cidade FOR EACH ROW EXECUTE PROCEDURE fAuditoria(); 
+CREATE TRIGGER trgAuditoriaProjeto AFTER INSERT OR UPDATE ON Projeto FOR EACH ROW EXECUTE PROCEDURE fAuditoria(); 
+CREATE TRIGGER trgAuditoriaMotivo AFTER INSERT OR UPDATE ON Motivo FOR EACH ROW EXECUTE PROCEDURE fAuditoria(); 
+CREATE TRIGGER trgAuditoriaVersao AFTER INSERT OR UPDATE ON Versao FOR EACH ROW EXECUTE PROCEDURE fAuditoria(); 
+CREATE TRIGGER trgAuditoriaPrioridade AFTER INSERT OR UPDATE ON Prioridade FOR EACH ROW EXECUTE PROCEDURE fAuditoria(); 
+CREATE TRIGGER trgAuditoriaFase AFTER INSERT OR UPDATE ON Fase FOR EACH ROW EXECUTE PROCEDURE fAuditoria(); 
+CREATE TRIGGER trgAuditoriaModulo AFTER INSERT OR UPDATE ON Modulo FOR EACH ROW EXECUTE PROCEDURE fAuditoria(); 
+CREATE TRIGGER trgAuditoriaCliente AFTER INSERT OR UPDATE ON Cliente FOR EACH ROW EXECUTE PROCEDURE fAuditoria(); 
+CREATE TRIGGER trgAuditoriaUsuario AFTER INSERT OR UPDATE ON Usuario FOR EACH ROW EXECUTE PROCEDURE fAuditoria(); 
+CREATE TRIGGER trgAuditoriaTarefa AFTER INSERT OR UPDATE ON Tarefa FOR EACH ROW EXECUTE PROCEDURE fAuditoria(); 
+CREATE TRIGGER trgAuditoriaTarefaUsuario AFTER INSERT OR UPDATE ON TarefaUsuario FOR EACH ROW EXECUTE PROCEDURE fAuditoria(); 
+CREATE TRIGGER trgAuditoriaMovimentoTarefa AFTER INSERT OR UPDATE ON TarefaUsuario FOR EACH ROW EXECUTE PROCEDURE fAuditoria(); 
