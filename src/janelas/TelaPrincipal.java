6945ffe685cccb9ece209d9257fc4a13cb9bb0e5
vogelleandro <vogelleandro@gmail.com>
2017-12-05 20:52:25 -0200

    V0.93
    Inicio do Socket.

--- src/janelas/TelaPrincipal.java
+++ src/janelas/TelaPrincipal.java
@@ -20,7 +20,7 @@ import entidade.Usuario;
 import entidade.Versao;
 import java.util.ArrayList;
 import java.util.Date;
-import javax.swing.JOptionPane;
+import sockets.Servidor;
 
 /**
  *
@@ -34,6 +34,8 @@ public class TelaPrincipal extends javax.swing.JFrame {
 //    Usuario usuario;
     public static Usuario userH = new Usuario();
     public static EnvioEmail envioEmail = new EnvioEmail();
+    Servidor servidor = null;
+    sockets.Cliente cliente = null;
     
     public TelaPrincipal() {
         initComponents();
@@ -48,6 +50,10 @@ public class TelaPrincipal extends javax.swing.JFrame {
         lblUsuario.setText(usuario.getLogin());
         userH = usuario;
         Validacao.setaPermissoes(this.jMenuBar1);
+        servidor = new Servidor("localhost",12345, null);
+        servidor.start();
+        cliente = new sockets.Cliente("224.0.0.0", 12345, txaErro , txaRetorno);
+        cliente.start();
     }
 
     /**
@@ -65,6 +71,11 @@ public class TelaPrincipal extends javax.swing.JFrame {
         lblVersao = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
         lblUsuario = new javax.swing.JLabel();
+        jScrollPane2 = new javax.swing.JScrollPane();
+        txaRetorno = new javax.swing.JTextArea();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        txaErro = new javax.swing.JTextArea();
+        jButton1 = new javax.swing.JButton();
         jMenuBar1 = new javax.swing.JMenuBar();
         menuArquivo = new javax.swing.JMenu();
         menuItemSair = new javax.swing.JMenuItem();
@@ -126,16 +137,48 @@ public class TelaPrincipal extends javax.swing.JFrame {
                     .addComponent(lblUsuario)))
         );
 
+        txaRetorno.setColumns(20);
+        txaRetorno.setRows(5);
+        jScrollPane2.setViewportView(txaRetorno);
+
+        txaErro.setColumns(20);
+        txaErro.setRows(5);
+        jScrollPane1.setViewportView(txaErro);
+
+        jButton1.setText("jButton1");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addComponent(jPanel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGap(72, 72, 72))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                        .addComponent(jButton1)
+                        .addGap(124, 124, 124))))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
-                .addGap(0, 391, Short.MAX_VALUE)
+                .addGap(40, 40, 40)
+                .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(33, 33, 33)
+                .addComponent(jButton1)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 85, Short.MAX_VALUE)
                 .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
         );
 
@@ -470,6 +513,11 @@ public class TelaPrincipal extends javax.swing.JFrame {
         }
     }//GEN-LAST:event_imnListaTarefasActionPerformed
 
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        servidor.definirMensagem("TESTE");
+            servidor.definirEnvio(true);
+    }//GEN-LAST:event_jButton1ActionPerformed
+
     /**
      * @param args the command line arguments
      */
@@ -523,6 +571,7 @@ public class TelaPrincipal extends javax.swing.JFrame {
     private javax.swing.JMenuItem imnCidades;
     private javax.swing.JMenuItem imnListaTarefas;
     private javax.swing.JMenuItem imnParametrosSistema;
+    private javax.swing.JButton jButton1;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JMenu jMenu1;
@@ -533,9 +582,13 @@ public class TelaPrincipal extends javax.swing.JFrame {
     private javax.swing.JMenuItem jMenuItem2;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JScrollPane jScrollPane2;
     private javax.swing.JLabel lblUsuario;
     private javax.swing.JLabel lblVersao;
     private javax.swing.JMenu menuArquivo;
     private javax.swing.JMenuItem menuItemSair;
+    private javax.swing.JTextArea txaErro;
+    private javax.swing.JTextArea txaRetorno;
     // End of variables declaration//GEN-END:variables
 }
